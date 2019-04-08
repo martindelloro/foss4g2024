79a014d73155bd1ae0c40a8e7c315fce4ab1f496
Martin Dell' Oro <martindell.oro@gmail.com>
2019-04-08 15:28:11 -0300

    Updates

---
+++ precache-manifest.55fb9a0e7b95299211de5ec39928fb33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff39b5308f314637044ae30881721525",
+    "url": "/foss4g2021/static/media/geoinquietos.ff39b530.png"
+  },
+  {
+    "revision": "19a7d37937b57f596a8b",
+    "url": "/foss4g2021/static/js/runtime~main.3a5f2358.js"
+  },
+  {
+    "revision": "8573dde1d4fe82cab14a",
+    "url": "/foss4g2021/static/js/main.f5519f9d.chunk.js"
+  },
+  {
+    "revision": "be3381f0cb8412dc160b",
+    "url": "/foss4g2021/static/js/2.b2558150.chunk.js"
+  },
+  {
+    "revision": "8573dde1d4fe82cab14a",
+    "url": "/foss4g2021/static/css/main.f50431c2.chunk.css"
+  },
+  {
+    "revision": "be3381f0cb8412dc160b",
+    "url": "/foss4g2021/static/css/2.6a54f420.chunk.css"
+  },
+  {
+    "revision": "5bb0d357c33e1561c3486ca6a9b9fd9d",
+    "url": "/foss4g2021/index.html"
+  }
+];
